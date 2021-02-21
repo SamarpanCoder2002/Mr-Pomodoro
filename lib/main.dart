8d7aa890855f718cf90d1d2fae9057fcecf31dfd
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2021-02-21 15:16:13 +0530

    Welcome Screen Modified and Responsive made

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_promodoro/FrontEnd/Welcome_screen.dart';
+import 'package:hello_promodoro/FrontEnd/welcomeScreen.dart';
 import 'package:hello_promodoro/FrontEnd/Main_Screen.dart';
 import 'package:hello_promodoro/FrontEnd/pomodoro_clock.dart';
 import 'package:hello_promodoro/FrontEnd/Sign_up.dart';
@@ -9,6 +9,6 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Mr. PromoDoro",
-    home: Welcome(),
+    home: Hello(),
   ));
 }
